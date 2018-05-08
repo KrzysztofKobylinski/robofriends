05f36c46cb4644366b44177adbaac0a1d39d91b5
kszysieg <krzysztof.a.kobylinski@gmail.com>
2018-05-08 14:17:57 +0200

    code cleanup

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 import './App.css'
 
 class App extends Component {
@@ -28,13 +28,14 @@ class App extends Component {
     }
 
     render() {
-        const filteredRobots = this.state.robots.filter(robot => {
+        const { robots, searchfield} = this.state;
+        const filteredRobots = robots.filter(robot => {
             return robot.name.toLowerCase()
-            .includes(this.state.searchfield.toLowerCase());
+            .includes(searchfield.toLowerCase());
         });
         console.log(filteredRobots);
         
-        if (this.state.robots.length ===0) {
+        if (!robots.length) {
             return <h1>Loading</h1>
         } else {
             return (
@@ -50,10 +51,7 @@ class App extends Component {
                     </Scroll>
                 </div>
             );
-
         }
-
-        
     }
 }
 
