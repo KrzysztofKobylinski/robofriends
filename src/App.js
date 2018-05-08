c1ec7b7a44428e006fb00bb45c222eae71f8beb8
kszysieg <krzysztof.a.kobylinski@gmail.com>
2018-05-08 13:15:06 +0200

    fully working search bar, with dynamic updates of robot list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import { robots } from './robots';
+import SearchBox from './SearchBox';
 
-const App = () => {
-    return (
-        <div>
-            <h1>Robofriends</h1>
-            <CardList robots={robots}/>
-        </div>
-    );
+
+class App extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+        
+    }
+
+    render() {
+
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase()
+            .includes(this.state.searchfield.toLowerCase());
+        });
+        console.log(filteredRobots);
+
+        return (
+            <div className='tc'>
+                <h1>Robofriends</h1>
+                <SearchBox 
+                    searchChange={this.onSearchChange} 
+                />
+                <CardList 
+                    robots={filteredRobots}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
